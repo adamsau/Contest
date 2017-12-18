db963e20e74793296894605c78f938ba7fec5ad9
Adams Au <adamsau@hotmail.com.hk>
2017-12-18 18:08:48 +0800

    Update BRCKTS.cpp

--- BRCKTS.cpp
+++ BRCKTS.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int sum[30005];
-int amin[30005];
+int sum[60005];
+int amin[60005];
 int n;
 
-void upate(int p) {
-	for(sum[p += n] *= -1; p > 0; p >>= 1) {
+void update(int p) {
+	for(amin[p + n] = sum[p + n] *= -1, p += n; p > 0; p >>= 1) {
 		sum[p >> 1] = sum[p] + sum[p ^ 1];
-		amin[p >> 1] = min(amin[p & 0], sum[p & 0] + amin[p | 1]);
+		amin[p >> 1] = min(amin[(p | 1) - 1], sum[(p | 1) - 1] + amin[p | 1]);
 	}
 }
 
@@ -19,8 +19,24 @@ void build() {
 	}	
 }
 
-int query(int l, int r) {
+int qsum(int l, int r) {
+	int s = 0;
+	for(l += n, r+= n; r > l; r >>= 1, l >>= 1) {
+		if(l & 1) s += sum[l++];
+		if(r & 1) s +=sum[--r];
+	}
 	
+	return s;
+}
+
+int qmin(int l, int r) {
+	int m = 60005;
+	for(l += n, r+= n; r > l; r >>= 1, l >>= 1) {
+		if(l & 1) m = min(m, amin[l++]);
+		if(r & 1) m = min(m, amin[--r]);
+	}
+	
+	return m;
 }
 
 void solve() {
@@ -33,6 +49,25 @@ void solve() {
 	}
 	
 	build();
+	
+	int m;
+	scanf("%d ", &m);
+	
+	for(int i = 0; i < m; ++i) {
+		int opr;
+		scanf("%d ", &opr);
+		if(opr > 0) {
+			update(opr - 1);
+		}
+		else {
+			int csum = qsum(0, n);
+			int cmin = qmin(0, n);
+			if(csum == 0 && cmin >= 0 && sum[n] == 1) {
+				printf("YES\n");
+			}
+			else printf("NO\n");
+		}
+	}
 }
 
 int main() {
