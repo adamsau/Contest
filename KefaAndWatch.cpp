d9125ab614b1e5c6e6f35d92481ea6a61b2c66ee
Adams Au <adamsau@hotmail.com.hk>
2018-01-10 15:56:34 +0800

    Update KefaAndWatch.cpp
    AC!

--- KefaAndWatch.cpp
+++ KefaAndWatch.cpp
@@ -18,7 +18,7 @@ YES
 using namespace std;
 
 struct T {
-	int h, int l, int r, int d;
+	int h, l, r, d;
 };
 
 int n, m, k, h;
@@ -28,7 +28,7 @@ int pw[100005], sum_pw[100005];
 int MD = 1e9 + 7, P = 4243;
 
 void apply(int p, int c) {
-	t[p] = sum_pw[t[p].r - t[p].l] * c % MD;
+	t[p].h = (long long) sum_pw[t[p].r - t[p].l] * c % MD;
 	if(p < n) t[p].d = c;
 }
 
@@ -45,7 +45,11 @@ void pushdown(int p) {
 }
 
 void pushup(int p) {
-	while(p > 1) p >>= 1, t[p].h = (t[p << 1].h * pw[t[p << 1 | 1].r - t[p << 1 | 1].l + 1] + t[p << 1 | 1].h) % MD;
+	while(p > 1) {
+		p >>= 1;
+		if(t[p].d == 0) t[p].h = ((long long) t[p << 1].h * pw[t[p << 1 | 1].r - t[p << 1 | 1].l + 1] + t[p << 1 | 1].h) % MD;
+		else t[p].h = (long long) sum_pw[t[p].r - t[p].l] * t[p].d % MD;
+	}
 }
 
 void update(int l, int r, int c) {
@@ -64,7 +68,7 @@ void update(int l, int r, int c) {
 	pushup(_r - 1);
 }
 
-void query(int l, int r) {
+int query(int l, int r) {
 	int _l = l, _r = r;
 	l += n, r += n;
 	
@@ -73,8 +77,8 @@ void query(int l, int r) {
 	
 	int hash = 0;
 	for(; l < r; l >>= 1, r>>= 1) {
-		if(l & 1) hash += t[l].h * pw[_r - 1 - t[l].r] % MD,hash %= MD, l++;
-		if(r & 1) --r, hash += t[r].h * pw[_r - 1 - t[r].r] % MD, hash %= MD;
+		if(l & 1) hash = (hash + (long long) t[l].h * pw[_r - 1 - t[l].r]) % MD, l++;
+		if(r & 1) --r, hash = (hash + (long long) t[r].h * pw[_r - 1 - t[r].r]) % MD;
 	}
 	
 	return hash;
@@ -87,16 +91,16 @@ int main() {
 	
 	int _n = n, _cnt = 0;
 	while(_n > 1) _cnt++, _n >>= 1;
-	h = cnt;
+	h = _cnt;
 	
 	pw[0] = 1;
 	sum_pw[0] = 1;
-	for(int i = 1; i < 100005; ++i) pw[i] = pw[i - 1] * P % MD, sum_pw[i] = sum_pw[i - 1] + pw[i] % MD;
+	for(int i = 1; i < 100005; ++i) pw[i] = (long long) pw[i - 1] * P % MD, sum_pw[i] = (long long) (sum_pw[i - 1] + pw[i]) % MD;
 	
 	for(int i = 0; i < n; ++i) t[n + i].h = s[i] - '0' + 1, t[n + i].l = i, t[n + i].r = i;
 	
 	for(int i = n - 1; i > 0; --i) {
-		t[i] = (t[i << 1] * pw[t[i << 1 | 1].r - t[i << 1 | 1].l + 1] + t[i << 1 | 1]) % MD;
+		t[i].h = ((long long) t[i << 1].h * pw[t[i << 1 | 1].r - t[i << 1 | 1].l + 1] + t[i << 1 | 1].h) % MD;
 		t[i].l = t[i << 1].l;
 		t[i].r = t[i << 1 | 1].r;
 	}
